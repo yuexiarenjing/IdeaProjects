19b8320a1876b339fd7da224fd9e086a455ecf0d
yuexiarenjing <yuexiarenjing@gmail.com>
2016-09-23 23:34:02 +0800

    first commit

---
+++ WindowForm1/src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Button btn = new Button();
+        btn.setText("Say 'Hello World!'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setTitle("Hello World！");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
